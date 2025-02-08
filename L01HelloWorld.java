b7f333224d2fef615a80257898e7184c3e83d329
MangoBond <21animasaunbondt@gravesendgrammar.com>
2025-02-08 13:49:13 +0000

    Added L01HelloWorld.java

---
+++ L01HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * 17/04/2024
+ * Author - Ayo Bond
+ * Program to display "Hello World!" in the console window
+ */
+
+public class L01HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}//end main
+
+}//end class
